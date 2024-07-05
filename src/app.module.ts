b491efd20d1a5c21dc959bc61f38b0bee30570d0
Daniel Suescun <profesionalsistemas14@pharmasan.co>
2024-07-04 23:37:40 -0500

    :zap: Creando modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+// Creando modulos para las dependencias
+// Comando: nest generate module <nombre_modulo>
+// Ejemplo corto: nest g mo auth
+import { TasksModule } from './tasks/tasks.module';
+import { ProjectsModule } from './projects/projects.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [TasksModule, ProjectsModule, AuthModule, UsersModule],
   controllers: [],
   providers: [],
 })
